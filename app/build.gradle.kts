b0e0f4115c3cfd3ad20a50c47f4ffb134dfb4cfa
Harry van der Wolf <harryvanderwolf@elitebook-845-g8.home>
2026-08-29 10:40:58 +0200

    update to java 21 for toolchain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
@@ -65,7 +65,7 @@ android {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
